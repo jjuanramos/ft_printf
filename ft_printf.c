1d48c5dc3ce7b422ca102fe260fe29539a7fc13f
jjuanramos <jjuanramos91@gmail.com>
2023-11-27 11:22:40 +0100

    more functions

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 11:01:54 by juramos           #+#    #+#             */
-/*   Updated: 2023/11/27 11:07:49 by juramos          ###   ########.fr       */
+/*   Updated: 2023/11/27 11:22:12 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	write_d(va_list args)
 	int	value;
 
 	value = va_arg(args, int);
-	write(1, ft_itoa(value), ft_strlen(ft_itoa(value)));
+	ft_putnbr_fd(value, 1);
 }
 
 void	write_c(va_list args)
@@ -26,6 +26,26 @@ void	write_c(va_list args)
 
 	value = (char)va_arg(args, int);
 	write(1, &value, 1);
+}
+
+void	write_u(va_list args)
+{
+	unsigned int	value;
+
+	value = (unsigned int)va_arg(args, int);
+	ft_putnbr_fd(value, 1);
+}
+
+void	write_s(va_list args)
+{
+	char	*value;
+
+	value = va_arg(args, char *);
+	while (*value)
+	{
+		write(1, value, 1);
+		*value++;
+	}
 }
 
 int	ft_printf(char const *format, ...)
@@ -44,6 +64,10 @@ int	ft_printf(char const *format, ...)
 				write_d(args);
 			else if (format[pos] == 'c')
 				write_c(args);
+			else if (format[pos] == 'u')
+				write_u(args);
+			else if (format[pos] == 's')
+				write_s(args);
 			else
 				write(1, "%", 1);
 		}
@@ -57,7 +81,7 @@ int	ft_printf(char const *format, ...)
 
 int	main(void)
 {
-	ft_printf("Hello %i, how you doing %c, %%\n", -1, 'b');
-	printf("Hello %i, how you doing %c, %%\n", -1, 'b');
+	ft_printf("Hello %i, how you doing %c, %%, %u, %s\n", 10010, 'b', 53, "bye juan");
+	printf("Hello %i, how you doing %c, %%, %u, %s\n", 10010, 'b', 45, "bye juan");
 	return (0);
 }

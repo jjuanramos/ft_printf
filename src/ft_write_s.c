72c63dc56d3640e4df881a43d24bd1b6b6606cb7
jjuanramos <jjuanramos91@gmail.com>
2023-12-07 11:45:09 +0100

    added way to handle null in string

--- src/ft_write_s.c
+++ src/ft_write_s.c
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 11:52:05 by juramos           #+#    #+#             */
-/*   Updated: 2023/12/07 10:48:00 by juramos          ###   ########.fr       */
+/*   Updated: 2023/12/07 11:42:36 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ size_t	ft_write_s(va_list args)
 
 	pos = 0;
 	value = va_arg(args, char *);
+	if (!value)
+		pos += write(1, "(null)", 6);
 	while (value[pos])
 	{
 		write(1, &value[pos], 1);

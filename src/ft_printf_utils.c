fed905eba99b11d91a22f890ad007634acc3be7d
jjuanramos <jjuanramos91@gmail.com>
2023-12-12 18:00:03 +0100

    removed unneeded if condition

--- src/ft_printf_utils.c
+++ src/ft_printf_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juramos <juramos@student.42.fr>            +#+  +:+       +#+        */
+/*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 10:01:49 by juramos           #+#    #+#             */
-/*   Updated: 2023/12/12 11:26:20 by juramos          ###   ########.fr       */
+/*   Updated: 2023/12/12 17:59:40 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,6 @@ size_t	putnbr_base_with_count_fd(unsigned long n, char *base, int fd)
 	size_t	i;
 
 	i = 0;
-	if (n < 0)
-	{
-		i += write(fd, "-", 1);
-		n *= -1;
-	}
 	if (n < (unsigned long)ft_strlen(base))
 		i += write(fd, &base[n], 1);
 	else
